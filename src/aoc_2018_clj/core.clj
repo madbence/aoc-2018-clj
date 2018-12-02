deb528779575c6e0a15a247c0a6f22d50d23f390
Bence Dányi <bence@danyi.me>
2018-12-02 22:58:26 +0100

    docs: document solution for day 2

--- src/aoc_2018_clj/core.clj
+++ src/aoc_2018_clj/core.clj
@@ -25,15 +25,15 @@
        (count (filter (partial has-freq? 3) lines)))))
 
 (defn p02b [lines]
-  (->> (for [a lines b lines] [a b])
-       (map #(apply map vector %))
-       (filter #(-> (filter (partial apply not=) %)
-                    (count)
-                    (= 1)))
-       (first)
-       (filter #(apply = %))
-       (map first)
-       (str/join)))
+  (->> (for [a lines b lines] [a b])                ; every possible pair from `lines`
+       (map #(apply map vector %))                  ; pairs of chars in each line pair
+       (filter #(-> (filter (partial apply not=) %) ; filter for pairs when they don't match
+                    (count)                         ; count them
+                    (= 1)))                         ; only line-pairs with 1 mismatching chars should be part of the result
+       (first)                                      ; take the first such line-pair
+       (filter #(apply = %))                        ; filter for matching char-pairs
+       (map first)                                  ; take the first char from those pairs (they are the same)
+       (str/join)))                                 ; join the the common chars into a string
 
 (defn -main
   "Advent of Code 2018"
